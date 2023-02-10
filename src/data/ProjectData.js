b6c1dde0eccaabf1cff20fc48911fe69b9b96a4e
Surya7286 <123283679+Surya7286@users.noreply.github.com>
2023-02-10 22:45:11 +0530

    Add files via upload

---
+++ src/data/ProjectData.js
@@ -0,0 +1,45 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "https://github.com/tGodson/car-rental-react/raw/development/app_screenshot.png",
+    title: "Admin dash board",
+    description: "Admin dash board” Angular JS | CSS | HTML | Bootstrap • Car Service Rental Website with Book Now, Form To accept Travel Details.",
+    tech_stack: "Angular, css, Bootstrap, Html ",
+    github_url: "https://github.com/Surya7286/Admin-dashboard",
+    demo_url: "https://github.com/Surya7286/Admin-dashboard",
+  },
+ 
+];
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
+    name: "ReactJS",
+  },
+
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+    name: "Responsive",
+  },
+  {
+    img: "https://angular.io/assets/images/logos/angular/angular.svg",
+    name: "Responsive",
+  },
+];
